dfe3865ef4d3f8bce03b1a7b66a16465e982e9c5
Michael <michael.ugbechie@capgemini.com>
2021-01-30 22:40:16 +0000

    insert

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "key_name" {
   default = "Michael-EU-West2"
   description = "SSH key name in your AWS account for AWS instances."
 }
+
+
+variable "ansible_pem_key" {
+  default = "Michael-EU-West2.pem"
+  description = "SSH key name in your AWS account for AWS instances."
+}
